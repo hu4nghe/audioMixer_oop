1102b59677a66193ba98304c23ee8c7a3759fda8
HUANG He <he.huang@utt.fr>
2023-12-14 13:11:03 +0100

    QTFF Parsing : Sample rate not found

--- include/audioMixer/audioMixer_QTFF_Parser.h
+++ include/audioMixer/audioMixer_QTFF_Parser.h
@@ -15,7 +15,9 @@ constexpr std::uint32_t moovCode = 0x6D6F6F76;
 constexpr std::uint32_t trakCode = 0x7472616B;
 constexpr std::uint32_t sounCode = 0x736F756E;
 
-std::string getStr(std::uint32_t code)
+using sPtrFile = std::shared_ptr<std::ifstream>;
+
+static std::string getStr(std::uint32_t code)
 {
     std::bitset<32> bits(code);
     std::string result;
@@ -24,48 +26,54 @@ std::string getStr(std::uint32_t code)
     return result;
 }
 
-
 class atom
 {
 protected:
-	std::uint32_t   size;
-	std::uint32_t   type;
-    std::streampos  pos;
-public:
-    atom(std::ifstream& file)
+	std::uint32_t  size;
+	std::uint32_t  type;
+    std::size_t    posBegin;
+    sPtrFile       file;
+private:
+    std::uint32_t read32bit_bigEndian()
     {
+        std::uint32_t rawData = 0;
+        file->read(reinterpret_cast<char*>(&rawData), sizeof(std::uint32_t));
+        rawData = ((rawData & 0xFF) << 24) | (((rawData >> 8) & 0xFF) << 16) | (((rawData >> 16) & 0xFF) << 8) | ((rawData >> 24) & 0xFF);
+        return rawData;
+    }
+public:
+    atom(sPtrFile fileStream)
+    {
+        file = fileStream;
         try
         {
-            pos = file.tellg();
-            file.read(reinterpret_cast<char*>(&size), sizeof(std::uint32_t));
-            file.read(reinterpret_cast<char*>(&type), sizeof(std::uint32_t));
-            type = ((type & 0xFF) << 24) | (((type >> 8) & 0xFF) << 16) | (((type >> 16) & 0xFF) << 8) | ((type >> 24) & 0xFF);
-            size = ((size & 0xFF) << 24) | (((size >> 8) & 0xFF) << 16) | (((size >> 16) & 0xFF) << 8) | ((size >> 24) & 0xFF);
+            posBegin = file->tellg();
+            size = read32bit_bigEndian();
+            type = read32bit_bigEndian();
+
             std::print("type : {}\nsize : {}\n", this->getTypeStr(), size);
+            std::print("current atom head : {}\n", posBegin);
+            std::print("current atom end  : {}\n", posBegin + size);
         }
         catch (const std::exception& e)
         {
             std::print(stderr, "Error : {}.\n", e.what());
             throw std::runtime_error("Failed to read file.");
         }
-        
     }
    ~atom() = default;
 
-    void gotoEnd(std::ifstream& file) const // declared const, but do modify the position of file pointer.
+    void skip()
     {
-        file.seekg(pos);
-        int i = file.tellg();
-        std::print("current atom head : {}\n", i);
-        file.seekg(size, std::ios::cur); 
-        i = file.tellg();
-        std::print("current atom end  : {}\n", i);
+        file->seekg(posBegin);
+        file->seekg(size, std::ios::cur);
     }
-
-    
+    bool typeIs (std::string_view fourCC) const { return fourCC == this->getTypeStr(); }
 
     [[nodiscard]] std::uint32_t getType   () const noexcept{ return type; }
     [[nodiscard]] std::uint32_t getSize   () const noexcept{ return size; }
+    [[nodiscard]] std::uint32_t getHead   () const noexcept{ return posBegin; }
+    [[nodiscard]] std::uint32_t getEnd    () const noexcept{ return posBegin + size; }
     [[nodiscard]] std::string   getTypeStr() const noexcept
     {
         std::bitset<32> bits(type);
@@ -74,36 +82,37 @@ public:
             result += static_cast<char>((bits >> i).to_ulong() & 0xFF);
         return result;
 	}
+};
 
-    bool typeIs(std::string_view fourCC) const
-    {
-        if (fourCC.size() != 4)
-            throw std::invalid_argument("Input FourCC must have exactly four characters.");
-        return fourCC == this->getTypeStr();
-    }
+class mp4aAtom : public atom
+{
+
+public:
+    mp4aAtom(sPtrFile file) : atom(file) {}
 };
 
 class QTFF
 {
-    std::ifstream fileStream;
+    sPtrFile fileStream;
 
     std::uint32_t readBigEndian()
     {
-        std::uint32_t target = 0;
-        fileStream.read(reinterpret_cast<char*>(&target), sizeof(target));
-        target = ((target & 0xFF) << 24) | (((target >> 8) & 0xFF) << 16) | (((target >> 16) & 0xFF) << 8) | ((target >> 24) & 0xFF);
-        return target;
+        std::uint32_t rawData = 0;
+        fileStream->read(reinterpret_cast<char*>(&rawData), sizeof(rawData));
+        rawData = ((rawData & 0xFF) << 24) | (((rawData >> 8) & 0xFF) << 16) | (((rawData >> 16) & 0xFF) << 8) | ((rawData >> 24) & 0xFF);
+        return rawData;
     }
 public:
     QTFF(const std::string& filePath)
     {
         try
         {
+            fileStream = std::make_shared<std::ifstream>();
             fs::path qtffPath(filePath);
             if (!fs::exists(qtffPath))
                 throw std::runtime_error("Invalid file path.");
-            fileStream.open(filePath, std::ios::binary);
-            if (!fileStream.is_open())
+            fileStream->open(filePath, std::ios::binary);
+            if (!fileStream->is_open())
                 throw std::runtime_error("Failed to open file.");
             atom fileType(fileStream);
             if (!fileType.typeIs("ftyp"))
@@ -111,45 +120,53 @@ public:
                 std::print("type is : {}", fileType.getTypeStr());
                 throw std::runtime_error("Not a valid QTFF file.");
             }
-            fileType.gotoEnd(fileStream);
+            fileType.skip();
         }
         catch (const std::exception& e)
         {
             throw std::runtime_error(std::format("Failed to open QTFF file : {}.", e.what()));
         }
     }
-   ~QTFF() { fileStream.close(); }
-    [[nodiscard]] std::size_t getPos() { return fileStream.tellg(); }
+   ~QTFF() { fileStream->close(); }
+    [[nodiscard]] std::size_t getPos() { return fileStream->tellg(); }
 
-    atom searchAtom(std::string_view code)
+    atom searchAtom     (std::string_view code)
     {
-        while (fileStream.good())
+        while (fileStream->good())
         {
             atom header(fileStream);
-            if (!header.typeIs(code))
-                header.gotoEnd(fileStream);
-            else
+            if (header.typeIs(code))
                 return header;
+            else
+                header.skip();
         }
         throw std::runtime_error("Atom not found.");
     }
-
     void searchAudioInfo()
     {
         auto moovAtom = searchAtom("moov");
-        size_t pos = fileStream.tellg();
-        size_t moovEndPos = pos + moovAtom.getSize();
-        while (fileStream.tellg() < moovEndPos)
+        size_t posBegin = fileStream->tellg();
+        size_t moovEndPos = posBegin + moovAtom.getSize();
+        while (fileStream->tellg() < moovEndPos)
         {
-            auto trakAtom = searchAtom("trak");
+            auto vtrakAtom = searchAtom("trak");
+            vtrakAtom.skip();
+            auto strakAtom = searchAtom("trak");
             auto mdiaAtom = searchAtom("mdia");
-            auto hdlrAtom = searchAtom("hdlr");
-
-            auto hdlrSubType = readBigEndian();
-            fileStream.seekg(8, std::ios::cur);
+            auto minfAtom = searchAtom("minf");
+            auto stblAtom = searchAtom("stbl");
+            auto stsdAtom = searchAtom("stsd");
             
-            if (getStr(hdlrSubType) == "soun")
-                std::print("sound track found.\n");
+            fileStream->seekg(4, std::ios::cur);
+            std::uint32_t nbentries = readBigEndian();
+            std::print("number of entries : {}\n", nbentries);
+            auto mp4aAtom = searchAtom("mp4a");
+            fileStream->seekg(40, std::ios::cur);
+            std::uint32_t channelNum = readBigEndian();
+            std::print("channelNum : {}\n",channelNum);
+
+            //mp4aAtom.skip();
+            break;
         }
     }
 };
--- include/audioMixer/audioMixer_base.h
+++ include/audioMixer/audioMixer_base.h
@@ -2,16 +2,19 @@
 #define AUDIOMIXER_BASE_H
 
 #include <cstdint>
-#include <memory>
+#include <memory>sPtrQueueList
 #include <print>
 
 #include "audioMixer_queue.h"
+
+template <audioType T>
+using sPtrQueueList = std::shared_ptr<std::vector<audioQueue<T>>>;
 
 class audioMixerModule_base
 {
 protected:
     //audioData
-    std::shared_ptr<std::vector<audioQueue<float>>> audio;
+    sPtrQueueList<float> audio;
     //Config, status
     outputParameter outputConfig;
                bool active;
@@ -33,17 +36,17 @@ public:
     virtual void start() = 0;
     virtual void stop () = 0;
 
-    [[nodiscard]] auto getAudio() const -> std::shared_ptr<std::vector<audioQueue<float>>> { return audio ; }
-    [[nodiscard]] bool isActive() const                                                    { return active; }
-};
-using module = audioMixerModule_base;
+    [[nodiscard]] auto getAudio() const -> sPtrQueueList<float> { return audio ; }
+    [[nodiscard]] bool isActive() const                         { return active; }
 
+    
+};
 /**
  * @brief
  *
  * @tparam T input module type : all derivated class of audioMixerModule_base, aka module.
  */
 template<typename U>
-concept inputMod_t = std::is_base_of<module, U>::value;
-
+concept inputMod_t = std::is_base_of<audioMixerModule_base, U>::value;
+using module = audioMixerModule_base;
 #endif//AUDIO_BASE_H
